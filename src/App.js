063a68587fa46a634d5c0602eac190658b3eee85
Tufail Mehraj <shahtuffey@gmail.com>
2022-04-18 17:24:19 +0530

    2 basic tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" data-test='counter-component'>
+      <h1 data-test="display-counter">The count is {count} </h1>
+      <button
+        data-test="increment-button"
+        className="button"
+        onClick={() => setCount(count + 1)}
         >
-          Learn React
-        </a>
-      </header>
+        Increment Counter
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import Enzyme, {configure, shallow} from "enzyme"
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({adapter: new EnzymeAdapter() })
+
+let wrapper;
+describe('testing click counter app', () => {
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+  test('renders without crashing', () => {
+    // const wrapper = shallow(<App />)
+    const component = wrapper.find(`[data-test="counter-component"]`);
+    expect(component.length).toBe(1)
+    // console.log(wrapper.debug())
+  });
+
+  test('Increment button renders', () => {
+    const button = wrapper.find('[data-test="increment-button"]')
+    expect(button.length).toBe(1);
+  })
+
+  test('render count display', () => {
+    const counter = wrapper.find('[data-test="display-counter"]')
+    expect(counter.length).toBe(1);
+  })
+
+  test('render count starts at zero', () => {
+
+  })
+
+
+  test('counter increments when button is clicked', () => {
+
+  })
+})
